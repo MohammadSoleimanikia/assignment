ca7382a702a74d3e68e3b54902228aba588b1efe
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-06-29 11:37:11 +0330

    change pic addresses

--- src/data.ts
+++ src/data.ts
@@ -2,44 +2,44 @@ import type { FoodCard } from "./components/shared/Card";
 
 export const FOOD_DATA: FoodCard[] = [
     {
-        PicAddress: "/public/Rectangle 29266-1.png",
+        PicAddress: "/Rectangle 29266-1.png",
         title: "خوشمزه خوشمزه",
         price: 2500001,
     },
 
     {
-        PicAddress: "/public/Rectangle 29266-2.png",
+        PicAddress: "/Rectangle 29266-2.png",
         title: "سرو غذا در ارتفاع",
         price: 2500001,
     },
     {
-        PicAddress: "/public/Rectangle 29266-1.png",
+        PicAddress: "/Rectangle 29266-1.png",
         title: "خوشمزه خوشمزه",
         price: 2500001,
     },
     {
-        PicAddress: "/public/Rectangle 29266.png",
+        PicAddress: "/Rectangle 29266.png",
         title: "تهچین خوری با امیر بلبلی",
         price: 2500001,
     },
     {
-        PicAddress: "/public/Rectangle 29266-1.png",
+        PicAddress: "/Rectangle 29266-1.png",
         title: "خوشمزه خوشمزه",
         price: 2500001,
     },
     {
-        PicAddress: "/public/Rectangle 29267.png",
+        PicAddress: "/Rectangle 29267.png",
         title: " کباب خوری بابا سلطان",
         price: 2500001,
     },
 
     {
-        PicAddress: "/public/Rectangle 29266-1.png",
+        PicAddress: "/Rectangle 29266-1.png",
         title: "خوشمزه خوشمزه",
         price: 2500001,
     },
     {
-        PicAddress: "/public/Rectangle 29268.png",
+        PicAddress: "/Rectangle 29268.png",
         title: " سه طعم جدید در بوم لانژ",
         price: 2500001,
     },
