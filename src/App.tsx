868ad12a774388a4455eabe41066782ce32e2dbc
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-06-30 05:47:19 +0330

    add user pick section desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import BestEvent from "./components/bestEvent/BestEvent";
 import Features from "./components/features/Features";
 import Hero from "./components/hero/Hero";
+import UserPicks from "./components/UserPicks/UserPicks";
 // import FreeEvents from "./components/freeEvents/FreeEvents";
 // import Testimonial from "./components/testimonial/Testimonial";
 // import TestimonialMobile from "./components/testimonial/TestimonialMobile";
@@ -9,16 +10,10 @@ function App() {
     return (
         <main className="bg-slate-100 min-h-screen space-y-24 font-vazir">
             <Hero />
-            <Features  />
-            <BestEvent />
+            <Features/>
+            <BestEvent/>
+            <UserPicks/>
             
-            {/* <FreeEvents/> */}
-            {/* <div className="hidden md:block"> */}
-                {/* <Testimonial /> */}
-            {/* </div> */}
-            {/* <div className="block md:hidden"> */}
-                {/* <TestimonialMobile /> */}
-            {/* </div> */}
         </main>
     );
 }
