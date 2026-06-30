ac484ec202e099182dadc74825fe95902279a633
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-06-30 16:12:55 +0330

    refactor feature layout

--- src/components/features/Features.tsx
+++ src/components/features/Features.tsx
@@ -28,73 +28,66 @@ export default function Features() {
     return (
         <div
             className="
-                relative
                 bg-gradient-to-br from-[#FF7777] to-[#EF234C]
-                pt-5
-                 lg:w-9/12 mx-auto lg:rounded-2xl
-                md:min-h-[300px]
+                lg:w-9/12 mx-auto lg:rounded-3xl py-5
+                md:min-h-[350px]
             "
         >
-            {/* desktop buttons - top left */}
-            <div className="hidden md:flex items-center gap-3 absolute top-5 left-5 z-30">
-                <SliderPrevButton onClick={handlePrevDesktop} />
-                <SliderNextButton onClick={handleNextDesktop} />
+            {/* heading */}
+            <div className="flex pt-5 mx-5 justify-between">
+                <div className="flex items-center">
+                    <h2
+                        className="text-lg text-white lg:text-2xl xl:text-2xl
+                                            md:font-semibold md:text-base "
+                    >
+                        از دستت در نره
+                    </h2>
+
+                    <img
+                        src="/ExclamationMark.svg"
+                        alt="exclamation mark"
+                        className="size-6"
+                    />
+                </div>
+                {/* desktop buttons - top left */}
+                <div className="hidden md:flex items-center gap-3 ">
+                    <SliderPrevButton onClick={handlePrevDesktop} />
+                    <SliderNextButton onClick={handleNextDesktop} />
+                </div>
+                {/* mobile buttons */}
+                <div className="flex md:hidden items-center gap-3">
+                    <SliderPrevButton onClick={handlePrevMobile} />
+                    <SliderNextButton onClick={handleNextMobile} />
+                </div>
             </div>
+            {/* main section */}
+            <div className="md:flex md:justify-between md:items-center md:flex-1">
+                {/* text */}
+                <div
+                    className="text-center text-white mt-3 mr-5 
+                                    lg:text-sm xl:text-lg
+                                    md:text-right md:mt-10 lg:px-3 space-y-3 "
+                >
+                    <p>رویداد های در حال برگزاری</p>
 
-            <div
-                className="
-                    flex flex-col md:flex-row
-                    space-y-2 md:space-y-0
-                    justify-between
-                    
-                    
-                "
-            >
-                {/* Info - right side on desktop */}
-                <div className="md:w-3/12 md:pr-2 text-white md:space-y-8">
-                    <div className="px-5 flex mt-5 justify-between relative">
-                        <div className="flex items-center">
-                            <h2 className="text-lg lg:text-2xl xl:text-2xl
-                                            md:font-semibold md:text-base ">
-                                از دستت در نره
-                            </h2>
-
-                            <img
-                                src="/ExclamationMark.svg"
-                                alt="exclamation mark"
-                                className="size-6"
-                            />
-                        </div>
-
-                        {/* mobile buttons */}
-                        <div className="flex md:hidden items-center gap-3">
-                            <SliderPrevButton onClick={handlePrevMobile} />
-                            <SliderNextButton onClick={handleNextMobile} />
-                        </div>
-                    </div>
-
-                    <div className="text-center
-                                    text-xs lg:text-sm xl:text-lg
-                                    md:text-right lg:px-3 space-y-3 ">
-                        <p>رویداد های در حال برگزاری</p>
-
-                        <p className="
+                    <p
+                        className="
                         text-xs lg:text-xs xl:text-md
-                        font-light">
-                            از امروز تا یک هفته بعد ...
-                        </p>
+                        font-light"
+                    >
+                        از امروز تا یک هفته بعد ...
+                    </p>
 
-                        <img
-                            src="/purple-clock.png"
-                            alt="purple clock icon"
-                            className="mr-8 mt-5 hidden md:block
-                            w-16 lg:w-20
+                    <img
+                        src="/purple-clock.png"
+                        alt="purple clock icon"
+                        className="mr-8 mt-5 hidden md:block
+                            w-16 lg:w-20 xl:w-28
                             xl:mb-3 drop-shadow-[12px_14px_25px_rgba(81,65,255,0.3)]"
-                        />
-                    </div>
+                    />
                 </div>
 
-                {/* Slider - left side on desktop */}
+                {/* slider */}
                 <div className="md:w-9/12 ">
                     <FeaturesSliderMobile
                         onSwiperReady={(swiper: SwiperType) => {
