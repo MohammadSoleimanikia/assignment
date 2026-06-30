7332f2704258be3fc309f02cb5a593c59c069fdc
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-06-30 10:03:25 +0330

    fix padding

--- src/components/hero/HeroSliderDesktop.tsx
+++ src/components/hero/HeroSliderDesktop.tsx
@@ -89,7 +89,7 @@ export default function HeroSliderDesktop() {
             </div>
 
             {/* active card data */}
-            <div className="px-10 pt-10 md:w-2/3 ">
+            <div className="px-10 pt-10 pb-5 md:w-2/3 ">
                 <div key={activeIndex} className="animate-cta-slide-up">
                     <div className="flex items-center gap-2">
                         <img
