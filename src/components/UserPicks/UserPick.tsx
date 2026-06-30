1e9587c891a9c19f1e50b8eea0f018a77334eb89
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-06-30 16:14:29 +0330

    change font size

--- src/components/UserPicks/UserPick.tsx
+++ src/components/UserPicks/UserPick.tsx
@@ -6,8 +6,8 @@ export default function UserPick() {
         <div className=" w-11/12 lg:w-9/12 mx-auto space-y-5" >
             {/* heading */}
             <div className="text-center space-y-2">
-                <h2 className="text-xl lg:text-4xl font-medium md:font-semibold ">انتخاب کاربران</h2>
-                <p className="md:text-[10px] text-sm lg:text-xl font-extralight ">تجربه هایی که حرف ندارن!</p>
+                <h2 className="text-xl 2xl:text-4xl font-medium md:font-semibold ">انتخاب کاربران</h2>
+                <p className="md:text-[10px] text-sm 2xl:text-xl font-extralight ">تجربه هایی که حرف ندارن!</p>
             </div>
             <UserPicksDesktop />
             <UserPicksMobile />
