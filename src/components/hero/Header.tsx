54c75fe914ca38fd0b289b077e096314762786d9
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-06-30 12:50:45 +0330

    change text for responsiveness

--- src/components/hero/Header.tsx
+++ src/components/hero/Header.tsx
@@ -2,15 +2,18 @@ export default function Header() {
     return (
         <div
             className="
-                        flex flex-col w-full justify-center 
-                        md:pr-30  items-center md:items-start md:pt-10 pt-4
+                        flex flex-col w-full justify-center space-y-3
+                        md:pr-30 2xl:pr-44
+                        md:pt-10 pt-4
+                        items-center md:items-start
                         text-white"
         >
-            <h1 className="text-4xl md:text-6xl font-bold drop-shadow-xl drop-shadow-black">
+            <h1 className="text-4xl md:text-6xl 2xl:text-8xl
+                    font-bold drop-shadow-xl drop-shadow-black">
                 غذا و رستوران
             </h1>
-            <div className="flex text-xs  mt-4">
-                <span className="bg-amber-300 rounded-lg px-1 py-0.5 ml-1 drop-shadow-xl drop-shadow-black">
+            <div className="flex text-xs lg:text-base mt-4">
+                <span className="bg-amber-300 rounded-lg px-1 py-0.5 ml-1 ">
                     132
                 </span>
                 <p>رویداد در این دسته بندی </p>
