8d9c7338cc36ba039ce7b691a5efe2c28128ae2f
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-06-30 09:22:33 +0330

    reArange items

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App() {
         <main className="bg-slate-100  font-vazir  min-h-screen space-y-24 font-vazir">
             <Hero />
             <Features />
-            <BestEvent />
             <UserPick />
+            <BestEvent />
         </main>
     );
 }
